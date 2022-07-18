2130b324ba667e3f00f0f2770d139a4a48c9e87c
Steven Yoon <67532994+yshun2272@users.noreply.github.com>
2022-07-17 22:36:46 -0400

    Add files via upload

---
+++ output.m
@@ -0,0 +1,22 @@
+filename = 'testdata.xls';
+
+Results_Orders = [customerorders.Data,expectedorders.Data,backlog.Data];
+Results_Desired = [desiredinventory.Data,desiredproduction.Data,desiredwip.Data];
+Results_Production = [first_productionstep.Data,first_outflow.Data,second_productionstep.Data,second_outflow.Data,third_productionstep.Data];
+Results_Utilization = [first_utilization.Data,second_utilization.Data,third_utilization.Data];
+Results_StockFlow = [productionstart.Data,wip.Data,production.Data,inventory.Data,shipments.Data];
+
+Labels_Orders = {'customerorders','expectedorders','backlog'};
+Labels_Desired = {'desiredinventory','desiredproduction','desiredwip'};
+Labels_Production = {'first_productionstep','first_outflow','second_productionstep','second_outflow','third_productionstep'};
+Labels_Utilization = {'first_utilization','second_utilization','third_utilization'};
+Labels_StockFlow = {'productionstart','wip','production','inventory','shipments'};
+
+Table_Orders = array2table(Results_Orders,'VariableNames',Labels_Orders);
+Table_Desired = array2table(Results_Desired,'VariableNames',Labels_Desired);
+Table_Production = array2table(Results_Production,'VariableNames',Labels_Production);
+Table_Utilization = array2table(Results_Utilization,'VariableNames',Labels_Utilization);
+Table_StockFlow = array2table(Results_StockFlow,'VariableNames',Labels_StockFlow);
+
+Table_All = [Table_Orders Table_Desired Table_Production Table_Utilization Table_StockFlow];
+writetable(Table_All,filename)
